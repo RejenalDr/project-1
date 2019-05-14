5fbb60854fd5c6846653824332be70061402d368
Denis <novosy@gmail.com>
2019-05-14 16:59:15 +0300

    second commit

--- project 1/main.js
+++ project 1/main.js
@@ -11,23 +11,64 @@ var appData = {
     savings: false
 };
 
-var requiredExpenses = prompt('Введите обязательную статью расходов в этом месяце', ''),
-    amount = +prompt('Во сколько обойдется?', ''),
-    requiredExpenses2 = prompt('Введите обязательную статью расходов в этом месяце', ''),
-    amount2 = +prompt('Во сколько обойдется?', '');
+var requiredExpenses = [],
+    amount = [];
 
-appData.expenses[requiredExpenses] = amount;
-appData.expenses[requiredExpenses2] = amount2;    
+for (var i = 0; i < 2; i++) {
+    requiredExpenses[i] = prompt('Введите обязательную статью расходов в этом месяце', '');
+    amount[i] = +prompt('Во сколько обойдется?', '');
 
+    if (typeof (requiredExpenses[i]) == 'string' && typeof (amount[i]) == 'number' && typeof (requiredExpenses[i]) != null && typeof (amount[i]) != null &&
+        requiredExpenses[i].length < 50) {
+        appData.expenses[requiredExpenses[i]] = amount[i];
+    } else {
+        alert('Введите значения обязательных расходов');
+        i--;
+    }
+}
+
+// var i = 0;
+// while (i < 2){
+//     requiredExpenses[i] = prompt('Введите обязательную статью расходов в этом месяце', '');
+//     amount[i] = +prompt('Во сколько обойдется?', '');
+
+//     if (typeof (requiredExpenses[i]) == 'string' && typeof (amount[i]) == 'number' && typeof (requiredExpenses[i]) != null && typeof (amount[i]) != null &&
+//         requiredExpenses[i].length < 50) {
+//         appData.expenses[requiredExpenses[i]] = amount[i];
+//         i++;
+//     } else {
+//         alert('Введите значения обязательных расходов');
+//     }
+// }
+
+// do {
+//     requiredExpenses[i] = prompt('Введите обязательную статью расходов в этом месяце', '');
+//     amount[i] = +prompt('Во сколько обойдется?', '');
+
+//     if (typeof (requiredExpenses[i]) == 'string' && typeof (amount[i]) == 'number' && typeof (requiredExpenses[i]) != null && typeof (amount[i]) != null &&
+//         requiredExpenses[i].length < 50) {
+//         appData.expenses[requiredExpenses[i]] = amount[i];
+//         i++;
+//     } else {
+//         alert('Введите значения обязательных расходов');
+//     }
+// } while (i < 2);
+
+appData.moneyPerDay = (appData.budget - (appData.expenses[requiredExpenses[0]] + appData.expenses[requiredExpenses[1]])) / 30;
 
 console.log(appData);
 
+alert("Бюджет на 1 день: " + appData.moneyPerDay);
 
-var oneDayBudget = (appData.budget - (appData.expenses[requiredExpenses] + appData.expenses[requiredExpenses2])) / 30;
-
-alert("Бюджет на 1 день" + oneDayBudget);
-
-
+if (appData.moneyPerDay < 100) {
+    console.log('минимальный уровень');
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 300) {
+    console.log('средний уровень');
+} else if (appData.moneyPerDay > 300){
+    console.log('высокый уровень');
+} else {
+    console.log('ошибка');
+}
 /*1. Сколько типов данных существует в JS? - 7: number, string, boolean, null, undefined, object, symbol 
 
 2. Как вывести информацию в консоль? - console.log();
